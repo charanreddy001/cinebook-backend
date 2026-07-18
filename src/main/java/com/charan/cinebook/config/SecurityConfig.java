4c72f96267eb026d2a6dfde3faa68f0470daa76a
charanreddy001 <charanreddypotham111@gmail.com>
2026-07-18 13:19:07 +0530

    db changed

--- src/main/java/com/charan/cinebook/config/SecurityConfig.java
+++ src/main/java/com/charan/cinebook/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://cinebook-project.netlify.app.netlify.app"
+                "https://cinebook-project.netlify.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
