4039ddd49e55d4da3c3b582212afd84aa40ab3d5
charanreddy001 <charanreddypotham111@gmail.com>
2026-07-18 12:38:29 +0530

    changed security code

--- src/main/java/com/charan/cinebook/security/JwtUtil.java
+++ src/main/java/com/charan/cinebook/security/JwtUtil.java
@@ -1,34 +1,33 @@
 package com.charan.cinebook.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final SecretKey key = Jwts.SIG.HS256.key().build();
+    private final SecretKey key;
     private final long expirationMs = 86400000;
 
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
     public String generateToken(String email, String role) {
-        return Jwts.builder()
-                .subject(email)
-                .claim("role",role)
+        return Jwts.builder().subject(email).claim("role", role)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expirationMs))
-                .signWith(key)
-                .compact();
+                .signWith(key).compact();
     }
 
     public String extractEmail(String token) {
-        return Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
     }
 
     public boolean isTokenValid(String token) {
@@ -41,11 +40,6 @@ public class JwtUtil {
     }
 
     public String extractRole(String token) {
-        return Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .get("role", String.class);
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
 }
